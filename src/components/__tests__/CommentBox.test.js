73fda899819e2b2e3580f81b35293c5cd11d0f43
Jedrzej.Lukasiuk <jlukasiuk@teonite.com>
2018-12-11 14:51:27 +0100

    Testing react with jest and enzyme

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(
+        <Root>
+            <CommentBox />
+        </Root>
+    );
+});
+
+afterAll(() => {
+    wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toBe(1);
+    expect(wrapped.find('button').length).toBe(1);
+});
+
+
+describe('text area', () => {
+    beforeEach(() => {
+        wrapped
+            .find('textarea').simulate('change', {
+                target: { value: 'new comment' }
+            })
+            .update();
+    });
+
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+    it('when form is submitted, text area gets emptied', () => {
+        wrapped
+            .find('form')
+            .simulate('submit')
+            .update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+});
